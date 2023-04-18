0254810f41ce586f7741e837c801c447a1f9591a
NanoSynth-java <comm2k3@gmail.com>
2023-04-18 22:17:06 +0200

    1st done

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[]{28, 12, 6, 40, 21, 18, 45, 34};
+        boolean sorted = false;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i + 1] < array[i]) {
+                    sorted = false;
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    System.out.println(Arrays.toString(array));
+                }
+            }
+        }
+    }
+}
