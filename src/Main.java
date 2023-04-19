caadfd01c1a2263ebd818077d327ad2feb392fd1
NanoSynth-java <comm2k3@gmail.com>
2023-04-19 21:30:12 +0200

    done something

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Logger;
-import java.util.logging.XMLFormatter;
+import java.util.logging.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         Logger logger = Logger.getLogger(Main.class.getName());
-        ConsoleHandler ch = new ConsoleHandler();
-        XMLFormatter xmlFormatter = new XMLFormatter();
-        ch.setFormatter(xmlFormatter);
-        logger.info("Test log");
-        logger.addHandler(ch);
+
+        try {
+            FileHandler fh = new FileHandler("%h/myJavaLog.log");
+            logger.addHandler(fh);
+            logger.setLevel(Level.ALL);
+
+        } catch (SecurityException e) {
+            logger.log(Level.INFO,
+                    "Error 1", e);
+        } catch (IOException e) {
+            logger.log(Level.INFO,
+                    "Error 2", e);
+        }
         int[] array = new int[]{28, 12, 6, 40, 21, 18, 45, 34};
         boolean sorted = false;
         while (!sorted) {
