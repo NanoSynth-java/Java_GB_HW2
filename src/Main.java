3f401b440c39e74ce79d91753f7c008c24c1f39b
NanoSynth-java <comm2k3@gmail.com>
2023-04-19 17:32:03 +0200

    1st done?

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 import java.util.Arrays;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Logger;
+import java.util.logging.XMLFormatter;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
+class Main {
+    static void myMainMethod(String[] args) {
+        Logger logger = Logger.getLogger(Main.class.getName());
+        ConsoleHandler ch = new ConsoleHandler();
+        XMLFormatter xmlFormatter = new XMLFormatter();
+        ch.setFormatter(xmlFormatter);
+        logger.info("Test log");
+        logger.addHandler(ch);
         int[] array = new int[]{28, 12, 6, 40, 21, 18, 45, 34};
         boolean sorted = false;
         while (!sorted) {
